dfa582770856e4dc635e7e0e435887bf985e1a85
Shrey Singh <125403535+9shrey@users.noreply.github.com>
2024-02-19 11:23:27 +0530

    Update q1.c

--- AL Lab/Lab7/q1.c
+++ AL Lab/Lab7/q1.c
@@ -93,66 +93,6 @@ struct Node *insertNode(struct Node *node, int key) {
   return node;
 }
 
-struct Node *minValueNode(struct Node *node) {
-  struct Node *current = node;
-
-  while (current->left != NULL)
-    current = current->left;
-
-  return current;
-}
-
-struct Node *deleteNode(struct Node *root, int key) {
-  if (root == NULL)
-    return root;
-
-  if (key < root->key)
-    root->left = deleteNode(root->left, key);
-
-  else if (key > root->key)
-    root->right = deleteNode(root->right, key);
-
-  else {
-    if ((root->left == NULL) || (root->right == NULL)) {
-      struct Node *temp = root->left ? root->left : root->right;
-
-      if (temp == NULL) {
-        temp = root;
-        root = NULL;
-      } else
-        *root = *temp;
-      free(temp);
-    } else {
-      struct Node *temp = minValueNode(root->right);
-      root->key = temp->key;
-      root->right = deleteNode(root->right, temp->key);
-    }
-  }
-  if (root == NULL)
-    return root;
-  root->height = 1 + max(height(root->left),
-               height(root->right));
-
-  int balance = getBalance(root);
-  if (balance > 1 && getBalance(root->left) >= 0)
-    return rightRotate(root);
-
-  if (balance > 1 && getBalance(root->left) < 0) {
-    root->left = leftRotate(root->left);
-    return rightRotate(root);
-  }
-
-  if (balance < -1 && getBalance(root->right) <= 0)
-    return leftRotate(root);
-
-  if (balance < -1 && getBalance(root->right) > 0) {
-    root->right = rightRotate(root->right);
-    return leftRotate(root);
-  }
-
-  return root;
-}
-
 void findPreSuc(struct Node* root, struct Node** pre, struct Node** suc, int key)
 {
     if (root == NULL)  return ;
@@ -203,16 +143,8 @@ int main() {
 	root = insertNode(root, 7);
 	root = insertNode(root, 4);
 	root = insertNode(root, 5);
-	root = insertNode(root, 3);
-	root = insertNode(root, 8);
-
+	
 	printPreOrder(root);
-
-	root = deleteNode(root, 3);
-
-	printf("\nAfter deletion: ");
-	printPreOrder(root);
-
 
 	struct Node* pre = NULL, *suc = NULL;
     printf("\nEnter key whose pre and suc to find: ");
@@ -230,3 +162,13 @@ int main() {
         printf("\nNo Successor");
     return 0;
 }
+
+
+//OUTPUT
+
+// 21547
+// Enter key to be searched: 3
+
+// Predecessor is 2
+
+// Successor is 4
