a93ef87921c1db040e74b155b203c703441eaeed
Shrey Singh <125403535+9shrey@users.noreply.github.com>
2024-02-23 10:59:56 +0530

    Add files via upload

--- AI Lab/Lab6/q1.py
+++ AI Lab/Lab6/q1.py
@@ -1,54 +1,38 @@
-SuccList = asarray([[]])
-Start='A'
+def f(x):
+    if -10 <= x <= 10:
+        return x
+    else:
+        return -1
 
-Closed = list()
-SUCCESS=True
-FAILURE=False
+def generate_neighbors(x):
+    neighbors = []
+    for i in [-1, 1]:
+        neighbor = x + i
+        if -10 <= neighbor <= 10:
+            neighbors.append(neighbor)
+    return neighbors
 
-def MOVEGEN(N):
-    New_list=list()
-    if N in SuccList.keys():
-        New_list=SuccList[N]
-    return New_list
-
-def SORT(L):
-    L.sort(key = lambda x: x[1])
-    return L
-
-def heu(Node): #Node = ['B',2]--> [Node[0],Node[1]]
-    return Node[1]
-
-
-def APPEND(L1,L2):
-    New_list=list(L1)+list(L2)
-    return New_list
-def Hill_Climbing(Start):
-    global Closed
-    N=Start
-    CHILD = MOVEGEN(N)
-    SORT(CHILD)
-    N=[Start,5]
-    print("\nStart=",N)
-    print("Sorted Child List=",CHILD)
-    newNode=CHILD[0]
-    CLOSED=[N]
-    while (heu(newNode) < heu(N)) and (len(CHILD) !=0):
-        print("\n--------------------------")
-        N= newNode
-        print("N=",N)
-        CLOSED = APPEND(CLOSED,[N])
-        CHILD = MOVEGEN(N[0])
-        SORT(CHILD)
-        print("Sorted Child List=",CHILD)
-        print("CLOSED=",CLOSED)
-        newNode=CHILD[0]
-    Closed=CLOSED 
+def hill_climbing(f, x0):
+    x = x0  # initial solution
+    best_solution = x0
+    while True:
+        neighbors = generate_neighbors(x)  # generate neighbors of x
+        if not neighbors:  # if there are no neighbors, stop
+            break
+        # find the neighbor with the highest function value
+        best_neighbor = max(neighbors, key=f)
+        if f(best_neighbor) > f(best_solution):  # if the best neighbor is better than the current best solution, update the best solution
+            best_solution = best_neighbor
+        else:  # if the best neighbor is not better than the current best solution, stop
+            break
+        print(best_neighbor)
+        x = best_neighbor  # continue with the best neighbor
+    return best_solution
 
 def main():
     a = int(input("Enter a number between -10 and 10: "))
-    b = hill_climbing(a<=10,a)
-    printf(b)
+    b = hill_climbing(f, a)
+    print(b)
 
-
-if __name__ =='__main__':
+if __name__ == '__main__':
     main()
